5b8a6ca1069b61e748ed550aeb91ccd6fa744460
festradasolano <festradasolano@gmail.com>
2014-09-30 11:30:41 -0500

    Modified mashment license from the MIT License to the Apache License, Version 2.0.

--- mashment-frontend/WebContent/maker/engine.js
+++ mashment-frontend/WebContent/maker/engine.js
@@ -1,9 +1,19 @@
 /*
- * Copyright (c) 2012-2014 Felipe Estrada-Solano <festradasolano at gmail>
- * Copyright (c) 2012      Oscar Mauricio Caicedo <omcaicedo at gmail>
- * Copyright (c) 2007-2009 Eric Abouaf <eric.abouaf at gmail>
+ * Copyright 2012-2014 Felipe Estrada-Solano <festradasolano at gmail>
+ * Copyright 2012      Oscar Mauricio Caicedo <omcaicedo at gmail>
+ * Originally created by Eric Abouaf, distributed under the MIT License
  * 
- * Distributed under the MIT License (MIT) (see LICENSE for details)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /**
@@ -11,11 +21,11 @@
  * wiring configurations that contains a set module instances and a set of wires
  * linking them. Each mashment has its own execution environment.
  * 
- * Copyright (c) 2012-2014 Felipe Estrada-Solano <festradasolano at gmail>
- * Copyright (c) 2012      Oscar Mauricio Caicedo <omcaicedo at gmail>
- * Copyright (c) 2007-2009 Eric Abouaf <eric.abouaf at gmail>
+ * Copyright 2012-2014 Felipe Estrada-Solano <festradasolano at gmail>
+ * Copyright 2012      Oscar Mauricio Caicedo <omcaicedo at gmail>
+ * Originally created by Eric Abouaf, distributed under the MIT License
  * 
- * Distributed under the MIT License (MIT) (see LICENSE for details)
+ * Distributed under the Apache License, Version 2.0
  */
 
 /* omcaicedo added this code: */
